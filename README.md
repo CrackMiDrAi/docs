a21c13787ae38b3432bb5755dc2c153e59ace306
Misa Liu <misaliu@misaliu.top>
2024-01-23 15:32:12 +0800

    更改非文档协议为 AGPLv3

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ npm run dev
 
 ## LICENSE
 
-[![CC BY-NC-SA 4.0 Badge](https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
+[![CC BY-NC-SA 4.0 Badge](https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png)](http://creativecommons.org/licenses/by-nc-sa/4.0/) [![AGPL 3.0 Badge](https://www.gnu.org/graphics/agplv3-88x31.png)](https://www.gnu.org/licenses/agpl-3.0)
 
-本仓库的非文档相关内容遵循 [MIT License](https://mit-license.org/)，文档相关内容遵循 [CC BY-NC-SA 4.0](http://creativecommons.org/licenses/by-nc-sa/4.0/) 协议
+本仓库的非文档相关内容遵循 [AGPL 3.0](https://www.gnu.org/licenses/agpl-3.0)，文档相关内容遵循 [CC BY-NC-SA 4.0](http://creativecommons.org/licenses/by-nc-sa/4.0/) 协议
--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -19,9 +19,9 @@ export default hopeTheme({
   navbar,
   sidebar,
 
-  copyright: 'Copyright © CrackMiDrAi Team & All contributors',
-  license: '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>',
-  footer: '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank"><img src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" alt="CC BY-NC-SA 4.0 Badge" /></a>',
+  copyright: '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank"><img src="https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png" alt="CC BY-NC-SA 4.0 Badge" /></a>&nbsp;<a href="https://www.gnu.org/licenses/agpl-3.0" target="_blank"><img src="https://www.gnu.org/graphics/agplv3-88x31.png" alt="AGPL 3.0 Badge" /></a>',
+  license: '<a href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA 4.0</a>&nbsp;|&nbsp;<a href="https://www.gnu.org/licenses/agpl-3.0" target="_blank">AGPL 3.0</a>',
+  footer: `CrackMiDrAi Team 2021-${(new Date()).getFullYear()}`,
   displayFooter: true,
 
   // hotReload: true,
